1b9a70dd75fa1f897085093b5d14a5473ab50c09
vishalcanopus <vishal.kohad@canopusinfosystems.in>
2020-01-21 15:37:13 +0530

    first passport commit

---
+++ routes/api/users.js
@@ -0,0 +1,104 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const router = require('express').Router();
+const auth = require('../auth');
+const Users = mongoose.model('Users');
+const client = require('../../config/redis.js');
+
+//POST new user route (optional, everyone has access)
+router.post('/insert', auth.optional, (req, res, next) => {
+  // const { body: { user } } = req;
+  const user = req.body;
+
+  if(!user.email) {
+    return res.status(422).json({
+      errors: {
+        email: 'is required',
+      },
+    });
+  }
+
+  if(!user.password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required',
+      },
+    });
+  }
+
+  const finalUser = new Users(user);
+
+  finalUser.setPassword(user.password);
+
+  return finalUser.save()
+    .then(() => res.json({ user: finalUser.toAuthJSON() }));
+});
+
+//POST login route (optional, everyone has access)
+router.post('/login', auth.optional, (req, res, next) => {
+  // const { body: { user } } = req;
+  const user = req.body;
+
+  if(!user.email) {
+    return res.status(422).json({
+      errors: {
+        email: 'is required',
+      },
+    });
+  }
+
+  if(!user.password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required',
+      },
+    });
+  }
+
+  return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
+    if(err) {
+      return next(err);
+    }
+    if(passportUser) {
+      const user = passportUser;
+      user.token = `${'token '}`+passportUser.generateJWT();
+      const logInUser = new Users(user);
+      logInUser.save()
+    .then((resp) => {
+      client.set(logInUser._id, resp, function(err, reply) {
+        console.log(reply);
+      });
+      // console.log('resp',resp)
+    });
+      return res.json({ user: user.toAuthJSON() });
+    }
+
+    return status(400);
+  })(req, res, next);
+});
+
+// router.post('/login',
+//   passport.authenticate('local',{ session: false }),
+//   function(req, res) {
+//     // If this function gets called, authentication was successful.
+//     // `req.user` contains the authenticated user.
+//     console.log('req.user',req.user)
+//     res.redirect('/users/' + req.user);
+//   }); 
+
+//GET current route (required, only authenticated users have access)
+router.get('/current', auth.required, (req, res, next) => {
+  const { payload: { id } } = req;
+  client.get(id, function(err, reply) {
+    console.log('user',reply);
+});
+  // return Users.findById(id)
+  //   .then((user) => {
+  //     if(!user) {
+  //       return res.sendStatus(400);
+  //     }
+  //     return res.json({ user: user.toAuthJSON() });
+  //   });
+});
+
+module.exports = router;
---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('express-jwt');
+const mongoose = require('mongoose');
+const Users = mongoose.model('Users');
+
+const getTokenFromHeaders = (req) => {
+  // const { headers: { authorization } } = req;
+  const authorization=req.headers.token;
+  // if(authorization && authorization.split(' ')[0] === 'token') {
+  //   return authorization.split(' ')[1];
+  // }
+  if(authorization) {
+    return authorization;
+  }
+      return null;
+};
+
+const auth = {
+  required: jwt({
+    secret: 'secret',
+    userProperty: 'payload',
+    getToken: getTokenFromHeaders,
+  }),
+  optional: jwt({
+    secret: 'secret',
+    userProperty: 'payload',
+    getToken: getTokenFromHeaders,
+    credentialsRequired: false,
+  }),
+};
+
+module.exports = auth;
