cb26178c7ed25931d4e5394ec0eacab49d89d785
Geetesh Devda <dgeetesh@gmail.com>
2020-01-25 15:33:28 +0530

    port

--- config/redis.js
+++ config/redis.js
@@ -1,6 +1,8 @@
 var redis = require('redis');
-var client = redis.createClient(); //creates a new client
-// var client = redis.createClient(port, host); //if need to add the post
+// var client = redis.createClient(); //creates a new client
+let port='18429';
+let host='ec2-35-173-89-108.compute-1.amazonaws.com';
+var client = redis.createClient(port, host); //if need to add the post
 
 client.on('connect', function() {
   console.log('Redis Connected');
