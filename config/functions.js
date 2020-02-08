1966625e9cbac514151836d342f972e4cb1699f6
Geetesh Devda <dgeetesh@gmail.com>
2020-02-08 21:51:30 +0530

    facebookLogin

---
+++ config/functions.js
@@ -0,0 +1,12 @@
+const findOne = (data) => {
+  let finaldata=JSON.parse(JSON.stringify(data));
+  let json={};
+  Object.keys(finaldata).forEach(function(key) {
+    if(key !== 'salt' && key !== 'hash' ){
+      json[key]=finaldata[key];
+    }
+  });
+  return json;
+};
+
+module.exports = findOne;
